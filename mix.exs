0fc29e7bbeb6d16621194ebcbadbd4d140dd8643
Sebastian <sebastian.callh@gmail.com>
2016-12-31 10:58:22 +0100

    forgot version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirLinearAlgebra.Mixfile do
 
   def project do
     [app: :elixir_linear_algebra,
-     version: "0.9.0",
+     version: "0.9.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
